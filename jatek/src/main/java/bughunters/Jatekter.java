7f74cf017e086c9fbdd45a5cccd5413cba4786ab
Tamás Gál <tamas.gal2003@gmail.com>
2025-03-21 14:03:36 +0100

    Jatekter2

--- jatek/src/main/java/bughunters/Jatekter.java
+++ jatek/src/main/java/bughunters/Jatekter.java
@@ -10,6 +10,8 @@ public class Jatekter {
         return tektonok;
     }
 
+    public Jatekter(){}
+
     public Jatekter(ArrayList<Tekton> t){
         tektonok = t;
     }
@@ -17,6 +19,13 @@ public class Jatekter {
     public void tores(int darab){
         System.out.println("Meghívódik a Jatekter tores metodusa.");
         tektonok.get(1).szomszedAllitas();
+        /*
+         * A szomszedAtallitas megkapja az újonnan létrehozott tektont,
+         * és mindkettőn megcsinálja a szomszédok átállítását.
+         * (azon is amin hívódik és azon is, amit megkapott paraméterként)
+         * 
+         * szomszedAtallitas(Tekton t)
+         */
 
         Tekton t4 = new Tekton();
         // szomszedok beállítása!!!!!!!!!
@@ -24,17 +33,34 @@ public class Jatekter {
         List<Gombafonal> ujszomszed = tektonok.get(1).gombafonalIgazitas();
 
         for (Gombafonal gombafonal : ujszomszed) {
-            if(gombafonal.getVegpont1() == tektonok.get(1)){
-                gombafonal.setVegpont1(t4);
+            if(gombafonal.getVeg1() == tektonok.get(1)){
+                gombafonal.setVeg1(t4);
             }
             else{
-                gombafonal.setVegpont2(t4);
+                gombafonal.setVeg2(t4);
             }
         }
 
         for (Gombafonal fonal : ujszomszed) {
             t4.addFonal(fonal);
         }
+
+
+        List<Tekton> t2szomszed = null;
+        t2szomszed.add(t4);
+        t2szomszed.add(tektonok.get(0));
+
+        tektonok.get(1).setSzomszedok(t2szomszed);
+
+
+        List<Tekton> t4szomszed = null;
+        t4szomszed.add(tektonok.get(1));
+        t4szomszed.add(tektonok.get(2));
+        
+        t4.setSzomszedok(t4szomszed);
+
+
+        tektonAdd(t4);
     }
     public void tektonAdd(Tekton t){
         System.out.println("Meghívódik a Jatekter tektonAdd metodusa.");
