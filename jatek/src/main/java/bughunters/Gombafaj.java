26aacba0aea3fe07657d05fac994b73bb15492fc
Jordán Panka Boglárka <jordanpankaboglarka@gmail.com>
2025-03-21 12:10:04 +0100

    Gombatest osztály implementálása.

--- jatek/src/main/java/bughunters/Gombafaj.java
+++ jatek/src/main/java/bughunters/Gombafaj.java
@@ -2,7 +2,7 @@ package bughunters;
 
 import java.util.ArrayList;
 
-public class Gombafaj{
+public class Gombafaj implements FonalKezeles{
     private String nev;
     private int termelesIdeje;
     private int eddigNovesztettTestekSzama;
@@ -19,26 +19,126 @@ public class Gombafaj{
         gombatestEletSzama=gtesz;
         gombaTestek=new ArrayList();
         gombafonalhalozat=new ArrayList();
+        System.out.println("Létrejött egy új gombafaj:" );
     }
+    public String getNev() {
+        return nev;
+    }
+    
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+    
+    public int getTermelesIdeje() {
+        return termelesIdeje;
+    }
+    
+    public void setTermelesIdeje(int termelesIdeje) {
+        this.termelesIdeje = termelesIdeje;
+    }
+    
+    public int getEddigNovesztettTestekSzama() {
+        return eddigNovesztettTestekSzama;
+    }
+    
+    public void setEddigNovesztettTestekSzama(int eddigNovesztettTestekSzama) {
+        this.eddigNovesztettTestekSzama = eddigNovesztettTestekSzama;
+    }
+    
+    public int getGombatestFejlettsegIdo() {
+        return gombatestFejlettsegIdo;
+    }
+    
+    public void setGombatestFejlettsegIdo(int gombatestFejlettsegIdo) {
+        this.gombatestFejlettsegIdo = gombatestFejlettsegIdo;
+    }
+    
+    public int getGombatestEletSzama() {
+        return gombatestEletSzama;
+    }
+    
+    public void setGombatestEletSzama(int gombatestEletSzama) {
+        this.gombatestEletSzama = gombatestEletSzama;
+    }
+    
+    public ArrayList<Gombatest> getGombaTestek() {
+        return gombaTestek;
+    }
+    
+    public void setGombaTestek(ArrayList<Gombatest> gombaTestek) {
+        this.gombaTestek = gombaTestek;
+    }
+    
+    public ArrayList<Gombafonal> getGombafonalhalozat() {
+        return gombafonalhalozat;
+    }
+    
+    public void setGombafonalhalozat(ArrayList<Gombafonal> gombafonalhalozat) {
+        this.gombafonalhalozat = gombafonalhalozat;
+    }
+    
     private void haldoklas(){
 
     }
-    //void  fonalNov(t1: Tekton, t2: Tekton){}
-    void addFonal(Gombafonal gf){
+    public void  fonalNov(Tekton hova, Tekton honnan){
+        System.out.println("Meghívódott a Gombafaj fonalNov metódusa.");
+        try{
+            Gombafonal uj= hova.gombafonalAdd(this,honnan);
+            addFonal(uj);
+            hova.addFonal(uj);
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        } 
+    }
+    public void addFonal(Gombafonal gf){
+        System.out.println("Meghívódik a gombafaj addFonal metódusa.");
+        if (gombafonalhalozat == null) {
+            gombafonalhalozat = new ArrayList<>();
+        }
         gombafonalhalozat.add(gf);
     }
-    void  fonalSzakad(Gombafonal gf){
+    public void  fonalSzakad(Gombafonal gf){
+        System.out.println("Meghivodik a gombafaj fonalSzakad metódusa.");
         //haldoklas
 
     }
-    //void testNovesztes(Tekton t){}
-    void addTest( Gombatest g){
-        gombaTestek.add(g);
+    public void testNovesztes(Tekton t){
+        System.out.println("Meghívódik a gombafaj testNovesztes metodusa.");
+        try{
+            Gombatest uj=t.gombatestNov(this);
+            addTest(uj);
+
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
-    //void sporaSzoras(Tekton t, Gombatest g){}
-    //boolean vanUt(Tekton a, Tekton b){}
-    //boolean lastChance(){}
-    void toresHaldoklas(){}
+    void addTest( Gombatest gt){
+        System.out.println("Meghívódik a gombafaj addTest metodusa.");
+        if (gombaTestek == null) {
+            gombaTestek = new ArrayList<>();
+        }
+        gombaTestek.add(gt);
+    }
+    public void sporaSzoras(Tekton t, Gombatest g){
+        System.out.println("Meghívódik a gombafaj sporaszoras metodusa.");
+        try
+        {
+            g.urit();
+            t.sporaSzor(this);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    public void lastChance(){
+        
+    }
+    public void toresHaldoklas(){
+        System.out.println("Meghívódik a gombafaj toresHaldoklas metodusa.");
+        haldoklas();
+    }
 
 
 }
--- jatek/src/main/java/bughunters/Gombatest.java
+++ jatek/src/main/java/bughunters/Gombatest.java
@@ -1,4 +1,45 @@
 package bughunters;
 public class Gombatest {
+    private int sporaRaktar;
+    private int kor;
+    private Gombafaj gombafaj;
+    public int getSporaRaktar() {
+        return sporaRaktar;
+    }
+    public Gombatest(){
+
+    }
     
+    public void setSporaRaktar(int sporaRaktar) {
+        this.sporaRaktar = sporaRaktar;
+    }
+    
+    public int getKor() {
+        return kor;
+    }
+    
+    public void setKor(int kor) {
+        this.kor = kor;
+    }
+    public void urit(){
+        System.out.println("Meghívódik a Gombatest urit metodusa.");
+    }
+    public Gombafaj getGombafaj() {
+        return gombafaj;
+    }
+    
+    public void setGombafaj(Gombafaj gombafaj) {
+        this.gombafaj = gombafaj;
+    }
+    
+    public void sporaGyujtes(){
+        System.out.println("Meghívódik a Gombatest sporaGyujtes metodusa.");
+
+    }
+    public void szorasTortent(){
+        System.out.println("Meghívódik a Gombatest szorasTortent metodusa.");
+    }
+    
+
+
 }
