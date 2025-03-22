c6cc2115e503f3b4106d6996d5401c3c28236c2c
Gál Tamás <tamas.gal2003@gmail.com>
2025-03-22 17:27:54 +0100

    Jatekter kommentezve

--- jatek/src/main/java/bughunters/Jatekter.java
+++ jatek/src/main/java/bughunters/Jatekter.java
@@ -3,43 +3,54 @@ package bughunters;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @brief A játéktér osztálya, amely a Tekton objektumokat kezeli.
+ */
 public class Jatekter {
     private List<Tekton> tektonok;
 
-    public List<Tekton> getTektonok(){
+    /**
+     * @brief Visszaadja a játéktérben lévő Tekton objektumokat.
+     * @return A tektonok listája.
+     */
+    public List<Tekton> getTektonok() {
         System.out.println("Meghívódik a Jatekter getTektonok metodusa.");
         return tektonok;
     }
 
-    public Jatekter(){
+    /**
+     * @brief Alapértelmezett konstruktor, amely létrehozza a Jatekter objektumot.
+     */
+    public Jatekter() {
         System.out.println("Meghívódik a Jatekter paraméter nélküli konstruktora.");
     }
 
-    public Jatekter(ArrayList<Tekton> t){
+    /**
+     * @brief Paraméteres konstruktor, amely egy meglévő Tekton listával inicializálja a játéktér objektumot.
+     * @param t A Tekton objektumokat tartalmazó lista.
+     */
+    public Jatekter(ArrayList<Tekton> t) {
         System.out.println("Meghívódik a Jatekter paraméteres konstruktora.");
         tektonok = t;
     }
 
-    public void tores(int darab){
+    /**
+     * @brief Egy új Tekton objektumot hoz létre és hozzáadja a játéktérhez.
+     * Emellett frissíti a kapcsolódó Gombafonal objektumokat.
+     * @param darab A törés során létrejövő új elemek száma (jelenleg nem használatos a metódusban).
+     */
+    public void tores(int darab) {
         Tekton t4 = new Tekton();
 
         System.out.println("Meghívódik a Jatekter tores metodusa.");
         tektonok.get(1).szomszedAllitas(t4);
-        /*
-         * A szomszedAtallitas megkapja az újonnan létrehozott tektont,
-         * és mindkettőn megcsinálja a szomszédok átállítását.
-         * (azon is amin hívódik és azon is, amit megkapott paraméterként)
-         * 
-         * szomszedAtallitas(Tekton t)
-         */
 
         List<Gombafonal> ujszomszed = tektonok.get(1).gombafonalIgazitas();
 
         for (Gombafonal gombafonal : ujszomszed) {
-            if(gombafonal.getVegpont1() == tektonok.get(1)){
+            if (gombafonal.getVegpont1() == tektonok.get(1)) {
                 gombafonal.setVegpont1(t4);
-            }
-            else{
+            } else {
                 gombafonal.setVegpont2(t4);
             }
         }
@@ -50,7 +61,12 @@ public class Jatekter {
 
         tektonAdd(t4);
     }
-    public void tektonAdd(Tekton t){
+
+    /**
+     * @brief Hozzáad egy új Tekton objektumot a játéktérhez.
+     * @param t Az új Tekton objektum.
+     */
+    public void tektonAdd(Tekton t) {
         System.out.println("Meghívódik a Jatekter tektonAdd metodusa.");
         tektonok.add(t);
     }
