ea4e2e6794b78aa99766d0483edf2dd462123ed2
Lehel <lehel886@gmail.com>
2025-03-22 19:21:36 +0100

    update main

--- jatek/src/main/java/bughunters/Main.java
+++ jatek/src/main/java/bughunters/Main.java
@@ -42,75 +42,63 @@ public class Main {
                     s.teszt7();
                     break;
                 case 8:
-                    s.setTestCase("Gombatest növesztése Tektonra, SIKERES");
+                    s.setTestCase("Gombatest növesztése Tektonra");
                     s.teszt8();
                     break;
                 case 9:
-                    s.setTestCase("Gombatest növesztése Tektonra, SIKERTELEN: nincs elég spóra a test növesztésére");
+                    s.setTestCase("Gombatest növesztése Tektonra, SIKERTELEN: nincs g1 típusú spóra a test növesztésére");
                     s.teszt9();
                     break;
                 case 10:
-                    s.setTestCase("Gombatest növesztése Tektonra, SIKERTELEN: nincs g1 típusú spóra a test növesztésére");
+                    s.setTestCase("Gombatest növesztése Tektonra, SIKERTELEN: van már gombatest t1 tektonon");
                     s.teszt10();
                     break;
                 case 11:
-                    s.setTestCase("Gombatest növesztése Tektonra, SIKERTELEN: van már gombatest t1 tektonon");
+                    s.setTestCase("Gombatest növesztése Puritektonra");
                     s.teszt11();
                     break;
                 case 12:
-                    s.setTestCase("Gombatest növesztése Puritektonra");
+                    s.setTestCase("Spóra szórása még nem létező Spórával");
                     s.teszt12();
                     break;
                 case 13:
-                    s.setTestCase("Spóra szórása már létező Spórával");
+                    s.setTestCase("Rovar mászik létező úttal");
                     s.teszt13();
                     break;
                 case 14:
-                    s.setTestCase("Spóra szórása még nem létező Spórával");
+                    s.setTestCase("Rovar mászik NEM létező úttal");
                     s.teszt14();
                     break;
                 case 15:
-                    s.setTestCase("Spóra szórása fejlett gombatest által");
+                    s.setTestCase("Gombafonal felszívódása Disszolátoron");
                     s.teszt15();
                     break;
                 case 16:
-                    s.setTestCase("Rovar mászik létező úttal");
+                    s.setTestCase("Fonal vágás");
                     s.teszt16();
                     break;
                 case 17:
-                    s.setTestCase("Rovar mászik NEM létező úttal");
+                    s.setTestCase("Bénító spóra evése");
                     s.teszt17();
                     break;
                 case 18:
-                    s.setTestCase("Gombafonal felszívódása Disszolátoron");
+                    s.setTestCase("T2 Tekton törés");
                     s.teszt18();
                     break;
                 case 19:
-                    s.setTestCase("Fonal vágás");
+                    s.setTestCase("Fonál lastChance újrakötött fonállal");
                     s.teszt19();
                     break;
                 case 20:
-                    s.setTestCase("Bénító spóra evése");
-                    s.teszt20();
-                    break;
-                case 21:
-                    s.setTestCase("Tekton törés");
-                    s.teszt21();
-                    break;
-                case 22:
-                    s.setTestCase("Fonál lastChance újrakötött fonállal");
-                    s.teszt22();
-                    break;
-                case 23:
                     s.setTestCase("Fonál lastChance nem újrakötött fonállal");
-                    s.teszt23();
+                    s.teszt20();
                     break;
                 case 0:
                     fut = false;
                     System.out.println("A program leáll...");
                     break;
                 default:
-                    System.out.println("Írjon be egy számot 1 és 23 között.");
+                    System.out.println("Írjon be egy számot 1 és 20 között.");
                     break;
             }
         }
@@ -138,23 +126,18 @@ public class Main {
         System.out.println("5. Gombafonal növesztése Monotektonra Gombafonalból, SIKERTELEN: m1-el jelen van másik gombafaj fonala");
         System.out.println("6. Gombafonal növesztése Tektonra Gombatestből, SIKERES");
         System.out.println("7. Gombafonal növesztése Tektonra Gombatestből, SIKERTELEN");
-        System.out.println("8. Gombatest növesztése Tektonra, SIKERES");
-        System.out.println("9. Gombatest növesztése Tektonra, SIKERTELEN: nincs elég spóra a test növesztésére");
-        System.out.println("10. Gombatest növesztése Tektonra, SIKERTELEN: nincs g1 típusú spóra a test növesztésére");
-        System.out.println("11. Gombatest növesztése Tektonra, SIKERTELEN: van már gombatest t1 tektonon");
-        System.out.println("12. Gombatest növesztése Puritektonra");
-        System.out.println("13. Spóra szórása már létező Spórával");
-        System.out.println("14. Spóra szórása még nem létező Spórával");
-        System.out.println("15. Spóra szórása fejlett gombatest által");
-        System.out.println("16. Rovar mászik létező úttal");
-        System.out.println("17. Rovar mászik NEM létező úttal");
-        System.out.println("18. Gombafonal felszívódása Disszolátoron");
-        System.out.println("19. Fonal vágás");
-        System.out.println("20. Bénító spóra evése");
-        System.out.println("21. Tekton törés");
-        System.out.println("22. Fonál lastChance újrakötött fonállal");
-        System.out.println("23. Fonál lastChance nem újrakötött fonállal");
+        System.out.println("8. Gombatest növesztése Tektonra");
+        System.out.println("9. Gombatest növesztése Tektonra, SIKERTELEN: nincs g1 típusú spóra a test növesztésére");
+        System.out.println("10. Gombatest növesztése Tektonra, SIKERTELEN: van már gombatest t1 tektonon");
+        System.out.println("11. Gombatest növesztése Puritektonra");
+        System.out.println("12. Spóra szórása még nem létező Spórával");
+        System.out.println("13. Rovar mászik létező úttal");
+        System.out.println("14. Rovar mászik NEM létező úttal");
+        System.out.println("15. Gombafonal felszívódása Disszolátoron");
+        System.out.println("16. Fonal vágás");
+        System.out.println("17. Bénító spóra evése");
+        System.out.println("18. T2 Tekton törés");
+        System.out.println("19. Fonál lastChance újrakötött fonállal");
+        System.out.println("20. Fonál lastChance nem újrakötött fonállal");
     }
-
-    
 }
