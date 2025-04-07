54da4baea1c0fea90c2002cc184a3b39faed475c
jordanpanka <jordanpankaboglarka@gmail.com>
2025-04-07 21:27:42 +0200

    Rovarász metódusainak felvétele

--- jatek/src/main/java/bughunters/Rovarasz.java
+++ jatek/src/main/java/bughunters/Rovarasz.java
@@ -5,7 +5,24 @@ import java.util.List;
 public class Rovarasz extends Jatekos {
     List<Rovar> rovarok;
 
+    public List<Rovar> getRovarok() {
+        return rovarok;
+    }
+
+    public void setRovarok(List<Rovar> rovarok) {
+        this.rovarok = rovarok;
+    }
+
     public Rovarasz(String nev):Jatekos(nev){
-        
+
+    }
+    public void vag(Gombafonal gf){
+
+    }
+    public void eszik(Tekton hova){
+
+    }
+    public void maszik(Spora sp){
+
     }
 }
