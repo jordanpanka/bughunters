8e99a7d34c5de8ffa63e3b2ebfe0f3492c622710
jordanpanka <jordanpankaboglarka@gmail.com>
2025-04-07 20:19:59 +0200

    Jatekos, Gombasz, Rovarasz

---
+++ jatek/src/main/java/bughunters/Gombasz.java
@@ -0,0 +1,10 @@
+package bughunters;
+
+public class Gombasz extends Jatekos{
+
+    private Gombafaj gombafaj;
+
+    Gombasz(String nev,Gombafaj gfaj):Jatekos(nev){
+        
+    }
+}
---
+++ jatek/src/main/java/bughunters/Rovarasz.java
@@ -0,0 +1,11 @@
+package bughunters;
+
+import java.util.List;
+
+public class Rovarasz extends Jatekos {
+    List<Rovar> rovarok;
+
+    public Rovarasz(String nev):Jatekos(nev){
+        
+    }
+}
