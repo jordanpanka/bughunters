08753092a1be3b8c0f1974eed8959bad7bf70b70
Páli Gréta Csillag <pali.greta.csillag@gmail.com>
2025-05-18 23:47:15 +0200

    Github bena

--- jatek/src/main/java/bughunters/Grafika/Grafika.java
+++ jatek/src/main/java/bughunters/Grafika/Grafika.java
@@ -135,8 +135,6 @@ public class Grafika extends JPanel {
 
         if (count == 0) return;
 
-<<<<<<< HEAD
-=======
         double angleStep = 2 * Math.PI / count;
         int radius = 30;  // dinamikus sugár
         int[] i = {0};
@@ -144,7 +142,6 @@ public class Grafika extends JPanel {
         rovarok.forEach((rovar, gg) -> {
             if (rovar.getTartozkodas().equals(t)) {
                 double angle = i[0] * angleStep - Math.PI / 2; // fentről induljon
->>>>>>> a12eb9b27b68414b7c38814749f378d6e5463aec
 
                 int x = (int)(cX + radius * Math.cos(angle));
                 int y = (int)(cY + radius * Math.sin(angle));
