2983d7edff2fa09e11bc0b70d27532b471cba586
Páli Gréta Csillag <pali.greta.csillag@gmail.com>
2025-05-19 13:20:34 +0200

    Nem tudom hogy változattam-e

--- jatek/src/main/java/bughunters/Grafika/Grafika.java
+++ jatek/src/main/java/bughunters/Grafika/Grafika.java
@@ -361,10 +361,10 @@ public class Grafika extends JPanel {
     public Spora sporaKeres(int x, int y){
         y=y-230;
          for (Map.Entry<Spora, GSpora> entry : sporak.entrySet()) {
-            double xC = (double)entry.getValue().getX()+3;
-            double yC = (double)entry.getValue().getY()+3;
+            double xC = (double)entry.getValue().getX()+12;
+            double yC = (double)entry.getValue().getY()+12;
             double d = Math.sqrt(Math.pow(x - xC, 2) + Math.pow(y - yC, 2));
-            if (d <=3) {
+            if (d <=12) {
                 return entry.getKey(); // megtaláltuk
             }
         }
