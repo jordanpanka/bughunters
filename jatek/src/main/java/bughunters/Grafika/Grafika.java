98786c5edce8faba2592ed3e490950ac72bc0407
Jordán Panka Boglárka <jordanpankaboglatka@gmail.com>
2025-05-19 10:28:53 +0200

    import

--- jatek/src/main/java/bughunters/Grafika/Grafika.java
+++ jatek/src/main/java/bughunters/Grafika/Grafika.java
@@ -3,8 +3,10 @@ package bughunters.Grafika;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.swing.JPanel;
 
